a3a5da2ee10434786a9297ccf8dbc8b0506d5e98
lincoln <lincoln@foxmail.com>
2018-10-14 21:27:32 +0800

    预应力旁站

--- pages/pangzhan/pangzhan_yylgz/pangzhan_yylgz.js
+++ pages/pangzhan/pangzhan_yylgz/pangzhan_yylgz.js
@@ -1,29 +1,47 @@
 
 var util = require('../../../utils/util.js')
+var math = require('../../../utils/math.js')
 const Toptips = require('../../../components/toptips/index.js');
 const Dialog = require('../../../components/dialog/dialog');
 Page({
   data: {
-
-    isColonShow: false,
-    currentTmpl: 0,
-    currentProcedure: 0,
-    procedureList: ['请选择', '主体结构', '建筑装饰装修', '屋面', '建筑给排水及供暖', '通风与空调', '建筑电气', '智能建筑', '建筑节能', '电梯'],
-
-    currentHjshPropList: [0, 0, 0],
-    hjshPropList: [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]],
-
+    
+    isColonShow : false,
     currentSedimentHeight: 0,
     sedimentHeightList: ['请选择', 40, 45, 50, 80, 90, 100, 280, 290, 300],
 
     currentSlurryProp: 0,
-    slurryPropList: ['请选择', 1.1, 1.2, 1.3],
+    slurryPropList: ['请选择', 1.1, 1.2,1.3],
 
-    currentActualSlump: 0,
+    currentActualSlump:0,
     actualSlumpList: ['请选择', 180, 190, 200, 210, 220],
-
     pang:{
-      tryDataUrl:[]
+      // id: 0,
+      // status: null,
+      // startTime: null,//监理开始时间
+      // building: null,
+      // pile: null,
+      // weather: null,
+      // endTime: null,//监理结束时间
+      // dropCageStartTime: null,//下钢筋笼时间起
+      // dropCageEndTime: null,//下钢筋笼时间止
+      // barCageCountImg: null,//钢筋笼照片
+      // secondCleanStartTime: null,//二次清孔时间起
+      // secondCleanEndTime: null,//二次清孔时间止
+      // actualDeepImg: null,//孔深照片
+      // actualDeep: null,//实际孔深
+      // sedimentHeight: null,//沉渣厚度
+      // slurryProp: null,//泥浆比重
+      // perfusionStartTime: null,//灌注时间起
+      // perfusionEndTime: null,//灌注时间止
+      // fillingCoefficient: null,//充盈系数
+      // actualVolume: null,//砼实灌方量
+      // actualSlump: null,//设计坍落度
+    },
+    tempImagesOfCage: [],
+    tempImagesOfHoleDepth: [],
+    result:{
+      barCageCount: 2,
     }
   },
   onLoad: function (options) {
@@ -35,17 +53,19 @@ Page({
     if (minute < 10) { minute = "0" + minute }
     this.setData({
       hour: hour,
-      minute: minute
+      minute: minute,
+      zongjian: wx.getStorageSync("zongjian"),
+      shigongfang: wx.getStorageSync("shigongfang"),
     })
-    // this.getShuiniTmpl();
-    
-    this.makeColonGlint(); 
-    if (options.pileStartNum) {
+    this.makeColonGlint();
+    // var options = {pileCode:1}
+    if (options.pileCode){
       this.setData({
-        ['pang.pileStartNum']: options.pileStartNum,
+        ['pang.pileCode']: options.pileCode,
+        ['buildingCode']: wx.getStorageSync("currentBuildingCode")
       })
       this.getOrCreate();
-    }   
+    }
   },
   onShow: function () {
 
@@ -60,48 +80,8 @@ Page({
 
 
   //------------------------------------------------------ff
-  /**
- * 监听模板变化
- */
-  onPickerChangeOfTmpl: function (e) {
-    if (e.detail.value == 0){
-      return;
-    }
-    this.setData({
-      currentTmpl: e.detail.value,
-      templateId: this.data.tmplList[e.detail.value].id
-    })
-    this.getOrCreate();
-    // this.updatePangzhan();
-  },
-  /**
-   * 水灰比
-   */
-  onPickerChangeOfHjshProp: function (e) {
-    // if (!this.data.pang.id) {
-    //   wx.showToast({
-    //     title: '旁站不存在',
-    //   })
-    //   return;
-    // }
-    console.log(e);
-    let that = this;
-    this.setData({
-      // slurryProp: that.data.procedureList[e.detail.value],
-      currentHjshPropList: e.detail.value
-    })
-    if (e.detail.value[0] != 0 && e.detail.value[1] != 0 && e.detail.value[1] != 0 ){
-        this.setData({
-          ['pang.hjshProp']: `${e.detail.value[0]},${e.detail.value[1]},${e.detail.value[2]}`
-        })
-    }else{
-      wx.showToast({
-        title: '数据错误',
-      })
-      return;
-    }
-    that.updatePangzhan();
-  },
+
+  // },
   /**
    * 天气选择
    */
@@ -117,22 +97,22 @@ Page({
     })
     this.updatePangzhan();
   },
-  
+
   //------------------------------------------------------ff
-  makeColonGlint: function () {
+  makeColonGlint:function(){
     let that = this;
-    setInterval(function () {
+    setInterval(function(){
       that.setData({
         isColonShow: !that.data.isColonShow
       })
-    }, 1000)
+    },1000)
     setInterval(function () {
       let date = new Date();
       let hour = date.getHours();
       let minute = date.getMinutes();
       console.log(hour, minute)
       if (hour < 10) { hour = "0" + hour }
-      if (minute < 10) { minute = "0" + minute }
+      if (minute < 10) { minute = "0" + minute}
       that.setData({
         hour: hour,
         minute: minute
@@ -140,12 +120,18 @@ Page({
     }, 60000)
 
   },
-
+  
   /**
    * 获得时间
    * case index,表示顺序序号
    */
-  getTime: function (e) {
+  getTime:function(e){
+    // if (!this.data.id) {
+    //   wx.showToast({
+    //     title: '旁站不存在',
+    //   })
+    //   return;
+    // }
     let prop = `pang.${e.currentTarget.dataset.index}`
     this.setData({
       [prop]: util.formatTime(new Date())
@@ -154,7 +140,7 @@ Page({
 
     this.updatePangzhan();
   },
- 
+
   preview: function (e) {
     let that = this;
     console.log(e);
@@ -165,90 +151,145 @@ Page({
     })
   },
 
-  /********************************************** 图片相关 ****************************************** */
+/********************************************** 图片相关 ****************************************** */
   /**
    * 上传照片
    */
   uploadImages: function (e) {
-    // if (!this.data.pang.id) {
-    //   wx.showToast({
-    //     title: '旁站不存在',
-    //   })
-    //   return;
-    // }
+    if (!this.data.pang.id) {
+      wx.showToast({
+        title: '旁站不存在',
+      })
+      return;
+    }
     let that = this;
     wx.chooseImage({
-      count: 6, // 默认9
+      count: e.currentTarget.dataset.index == "weldingUrlAndTime" ? 1 : 6, // 默认9
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
-        if (e.currentTarget.dataset.index == "tryDataUrl") {
-          let len1 = that.data.pang.tryDataUrl.length;
+        if (e.currentTarget.dataset.index == "lastTenHitLrrigationUrl"){
+          let len1 = that.data.pang.lastTenHitLrrigationUrl.length;
           let len2 = res.tempFilePaths.length;
           if (len2 > 8 - len1) {
             res.tempFilePaths.length = 9 - len1;
           }
           util.uploadImgPromises(res.tempFilePaths, function (imgs) {
-
             that.setData({
-              ['pang.tryDataUrl']: that.data.pang.tryDataUrl ? that.data.pang.tryDataUrl.concat(imgs) : imgs,
-              // actualDeepImg:null
+              ['pang.lastTenHitLrrigationUrl']: that.data.pang.lastTenHitLrrigationUrl ? that.data.pang.lastTenHitLrrigationUrl.concat(imgs) : imgs,
+            })
+          });
+        }
+        else if (e.currentTarget.dataset.index == "weldingUrlAndTime"){
+          // let len1 = that.data.pang.weldingUrlAndTime.length;
+          // let len2 = res.tempFilePaths.length;
+          // let time = util.formatTime(new Date());
+          // if (len2 > 8 - len1) {
+          //   res.tempFilePaths.length = 9 - len1;
+          // }
+          let time = util.formatTime(new Date());
+          util.uploadImgPromises(res.tempFilePaths, function (imgs) {
+            that.setData({
+              ['pang.weldingUrlAndTime']: that.data.pang.weldingUrlAndTime ? that.data.pang.weldingUrlAndTime.concat([{ url: imgs[0], time: time }]) : [{ url: imgs[0],time:time }]
+            })
+          });
+        }
+        else if (e.currentTarget.dataset.index == "deptRockUrl") {
+          let len1 = that.data.pang.deptRockUrl.length;
+          let len2 = res.tempFilePaths.length;
+          if (len2 > 8 - len1) {
+            res.tempFilePaths.length = 9 - len1;
+          }
+          util.uploadImgPromises(res.tempFilePaths, function (imgs) {
+            that.setData({
+              ['pang.deptRockUrl']: that.data.pang.deptRockUrl ? that.data.pang.deptRockUrl.concat(imgs) : imgs
             })
           });
         }
       }
     })
   },
-  updateImagesOrSave: function (e) {
+
+  updateImagesOrSave:function(e){
     let that = this;
     console.log(e);
-    if (e.currentTarget.dataset.index == "tryDataUrl") {
-      if (that.data.pang.tryDataUrl.length > 0) {
+    if (e.currentTarget.dataset.index == "lastTenHitLrrigationUrl") {
+      if (that.data.pang.lastTenHitLrrigationUrl.length > 0){
+        that.updatePangzhan();
+      }else{
+        wx.showToast({
+          title: '照片不能为空',
+        })
+      }
+      
+    }
+    else if (e.currentTarget.dataset.index == "weldingUrlAndTime"){
+      if (that.data.pang.weldingUrlAndTime.length > 0){
+        that.updatePangzhan();
+      }else{
+        wx.showToast({
+          title: '照片不能为空',
+        })
+      }
+    }
+    else if (e.currentTarget.dataset.index == "deptRockUrl") {
+      if (that.data.pang.deptRockUrl.length > 0) {
         that.updatePangzhan();
       } else {
         wx.showToast({
           title: '照片不能为空',
         })
       }
-
     }
   },
+
   deletePic: function (e) {
     console.log(e);
     let index = e.currentTarget.dataset.index;
     let stepindex = e.currentTarget.dataset.stepindex;
     let that = this;
-
+    
     console.log(that.data.tempFilePaths);
-    if (stepindex == "tryDataUrl") {//下钢筋笼
-      that.data.pang.tryDataUrl.splice(index, 1);
+    if (stepindex == "lastTenHitLrrigationUrl"){//
+      that.data.pang.lastTenHitLrrigationUrl.splice(index, 1);
       this.setData({
-        ['pang.tryDataUrl']: that.data.pang.tryDataUrl
+        ['pang.lastTenHitLrrigationUrl']: that.data.pang.lastTenHitLrrigationUrl
+      })
+    } else if (stepindex == "actualDeepImg") {//
+      that.data.pang.actualDeepImg.splice(index, 1);
+      this.setData({
+        ['pang.actualDeepImg']: that.data.pang.actualDeepImg
+      })
+    }
+    else if (stepindex == "deptRockUrl") {//
+      that.data.pang.deptRockUrl.splice(index, 1);
+      this.setData({
+        ['pang.deptRockUrl']: that.data.pang.deptRockUrl
       })
     }
   }, 
 
   bindTimeChange: function (e) {
-    // if (!this.data.pang.id) {
-    //   wx.showToast({
-    //     title: '旁站不存在',
-    //   })
-    //   return;
-    // }
+    if (!this.data.pang.id) {
+      wx.showToast({
+        title: '旁站不存在',
+      })
+      return;
+    }
     console.log(e);
     let date;
     let time = util.formatTime(new Date());
     switch (e.currentTarget.dataset.index) {
-      case 'finishPileStartTime':
-        date = this.data.finishPileStartTime.split(' ')[0];
+      case 'startTime':
+        date = this.data.startTime.split(' ')[0];
         this.setData({
-          finishPileStartTime: date + " " + e.detail.value + ":00"
+          startTime: date + " " + e.detail.value + ":00"
         })
         break;
-      case 'finishPileEndTime':
-        date = this.data.finishPileEndTime.split(' ')[0];
+      case 'endTime':
+        date = this.data.endTime.split(' ')[0];
         this.setData({
-          finishPileEndTime: date + " " + e.detail.value + ":00"
+          endTime: date + " " + e.detail.value + ":00"
         })
         break;
       case 'dropCageEndTime':
@@ -292,10 +333,9 @@ Page({
     this.updatePangzhan();
 
     console.log('picker发送选择改变，携带值为', e.detail.value);
-
+   
   },
-
-  // -----------------------------------------------------------------编辑框三个监听事件
+  // -----------------------------------------------------------------------------------------------------------------------------
   /**edit
    * 切换成可编辑状态
    */
@@ -305,7 +345,7 @@ Page({
       [isEdit]: true
     })
     return;
-
+    
   },
   /**
    * 取消编辑状态
@@ -313,17 +353,18 @@ Page({
   toDone: function (e) {
     let isEdit = `pang.is${e.currentTarget.dataset.index}Edit`
     this.setData({
-      [isEdit]: false
+      [isEdit] : false
     })
   },
   /**
    * 保存
    */
   onInputSave: function (e) {
-    if (e.currentTarget.dataset.index == "pileStartNum"){
-
-    }else{
-      // if (!this.data.pang.id) {
+    console.log(e);
+    //先输入楼号和桩号
+    if (e.currentTarget.dataset.index == 'pileCode' || e.currentTarget.dataset.index == 'buildingCode') {
+    } else {
+      // if (!this.data.id) {
       //   wx.showToast({
       //     title: '旁站不存在',
       //   })
@@ -332,31 +373,111 @@ Page({
     }
     let porp = `pang.${e.currentTarget.dataset.index}`
     let isEdit = `pang.is${e.currentTarget.dataset.index}Edit`
-
     //为空时不设置
-    if (!e.detail.value.value.trim()) {
+    if (!e.detail.value.value.trim()){
       this.setData({
         [porp]: null,
         [isEdit]: false
       })
-    } else {
+    }else{
       this.setData({
         [porp]: e.detail.value.value,
         [isEdit]: false
+      })  
+    }
+    // 输入楼号桩号决定旁站
+    if (e.currentTarget.dataset.index == 'pileCode' || e.currentTarget.dataset.index == 'buildingCode'){
+      this.getOrCreate();
+      return
+    }
+    let obj = this.data.pang;
+    // 公式计算
+    if (e.currentTarget.dataset.index == 'groundHeight' || e.currentTarget.dataset.index == 'pileTopHeight'){
+      this.setData({
+        songzhuangDeep: math.accSub(obj.groundHeight, obj.pileTopHeight),
       })
     }
-    if (e.currentTarget.dataset.index == "pileStartNum"){
-      this.getOrCreate();
-    }else{
-      this.updatePangzhan();
+    this.updatePangzhan();
+  },
+  /**
+   * 确认完成
+   */
+  submitToCheck: function () {
+    let that = this;
+    let data = {
+      type: "0001",
+      title: "温州机场重建项目 1号楼120号桩机械灌注旁站完成",
+      fromId: 77,
+      fromName: "1111",
+      toIds: [
+        55,
+        77,
+        78
+      ],
+      parameter: {
+        pangzhanId: 1
+      }
     }
+    util.getDataByAjax({//--首页商品列表
+      url: "/message/addJxgzz",
+      method: "Post",
+      data,
+      success: function (res) {
+        Toptips({
+          duration: 1000,
+          content: "成功提交",
+          backgroundColor: "#06A940"
+        });
+        setTimeout(function () {
+          wx.navigateBack({
+            delta: -1
+          })
+        }, 2000)
+      },
+      error: function () { }
+    });
   },
 
-  updatePangzhan: function () {
-    let data = this.data.pang;
+  getOrCreate: function(){
+    if (!this.data.buildingCode || !this.data.pang.pileCode){
+      return;
+    }
     let that = this;
+    util.getDataByAjax({ 
+      url: "/yylgzpz/getOrCreate",
+      method: "Post",
+      data: {
+        projectId: wx.getStorageSync("currentProjectId")-0,
+        buildingId: wx.getStorageSync("currentBuildingId") - 0,
+        pileCode: that.data.pang.pileCode - 0,
+      },
+      success: function (res) {
+        let obj = res.data.result ? res.data.result : {};
+        that.setData({
+          pang: obj,
+          abc1: math.accSub(math.accAdd(obj.platformElevation , obj.designPileLength) , (obj.pileTopHeight)),
+          abc2: math.accSub(math.accSub(obj.platformElevation, obj.pileTopHeight), 0.5),
+          ['pang.lastTenHitLrrigationUrl']: obj.lastTenHitLrrigationUrl ? JSON.parse(obj.lastTenHitLrrigationUrl) : [],
+          ['pang.weldingUrlAndTime']: obj.weldingUrlAndTime ? JSON.parse(obj.weldingUrlAndTime) : [],
+        })
+
+      },
+      error: function () {
+
+      }
+    });
+  },
+  updatePangzhan: function () {
+        let that = this;
+    if (!this.data.pang.mainBarNum || !this.data.pang.mainBarType) {
+      this.setData({
+        ['pang.mainBar']:null
+      })
+    }
+    let obj = this.data;
+    let data = this.data.pang;
     util.getDataByAjax({
-      url: '/snJbjPzjl/update',
+      url: '/yylgzpz/update',
       method: "Post",
       data,
       success: function (res) {
@@ -365,51 +486,14 @@ Page({
           content: "成功采集！",
           backgroundColor: "#06A940"
         });
-      }
-    });
-  },
-  getShuiniTmpl: function(){
-    let that = this;
-    util.getDataByAjax({
-      url: `/snJbjPzjlTemplate/list?pageIndex=1&pageSize=100`,
-      method: "Post",
-      data: {},
-      success: function (res) {
-        let tmpltNameList = ['请选择'];
-        for (let i = 0; i < res.data.result.list.length; i++) {
-          tmpltNameList.push(res.data.result.list[i].supplier);
-        }
-        that.setData({
-          tmplList: res.data.result.list,
-          tmpltNameList: tmpltNameList
-        })
-      }
-    });
-  },
-  getOrCreate: function () {
-    if (!this.data.pang.pileStartNum) {
-      return;
-    }
-    let that = this;
-    util.getDataByAjax({//--首页商品列表
-      url: "/snJbjPzjl/add",
-      method: "Post",
-      data: {
-        pileStartNum: that.data.pang.pileStartNum,
-        projectId: wx.getStorageSync('currentProjectId'),
+        // wx.showToast({
+        //   title: '成功采集！',
+        //   success: function () {
+        //   }
+        // })
       },
-      success: function (res) {
-        let obj = res.data.result;
-        that.setData({
-          pang: res.data.result,
-          ['pang.tryDataUrl']: obj.tryDataUrl ? JSON.parse(obj.tryDataUrl) : [],
-          // ['pang.barCageCountImg']: obj.barCageCountImg ? JSON.parse(obj.barCageCountImg) : [],
-          // ['pang.deptRockUrl']: obj.deptRockUrl ? JSON.parse(obj.deptRockUrl) : [],
-        })
-      },
-      error: function () {
-
-      }
+      error: function () { }
     });
   },
-})  
+  })
+  
